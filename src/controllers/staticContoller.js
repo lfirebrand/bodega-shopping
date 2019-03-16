e4cfe9f0670027e3b4a25f44721f8ab9ed883d9d
Lexi Zgud <lexibitesblog@gmail.com>
2019-03-16 09:16:26 -0500

    Added About view and routing

--- src/controllers/staticContoller.js
+++ src/controllers/staticContoller.js
@@ -1,5 +1,9 @@
 module.exports = {
   index(req, res, next) {
     res.render("static/index", { title: "Welcome to Bodega" });
+  },
+
+  about(req, res, next) {
+    res.render("static/about", { h1: "About" });
   }
 };
